90c1e211b1bf596cb483928daaa78bbff293a3a6
Adarsh <adarshkatiyar73@gmail.com>
2026-01-31 17:05:59 +0530

    authetication

---
+++ Client/src/Context/AppState.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import AppContext from './appContext'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const AppState = (props) => {
+
+    const url = 'http://localhost:1000/api/user'
+    const[Users,setUsers] = useState([])
+    const data = 10;
+    useEffect(()=>{
+        const fetchUser = async() =>{
+            const api = await axios.get(`${url}/allUser`,{
+                headers:{
+                    "Content-Type":"Application/json"
+                },
+                withCredentials: true
+            })
+            console.log(api.data.alluser);
+            setUsers(api.data.alluser);
+        }
+        fetchUser();
+    },[])
+
+    //register User
+    const register = async(name,email,password) =>{
+            const api = await axios.post(`${url}/register`,
+                {name,email,password},{
+                headers:{
+                    "Content-Type":"Application/json"
+                },
+                withCredentials: true
+            })
+          alert(api.data.message);
+          return api.data;
+        }
+         const login = async(email,password) =>{
+            const api = await axios.post(`${url}/login`,
+                {email,password},{
+                headers:{
+                    "Content-Type":"Application/json"
+                },
+                withCredentials: true
+            })
+          alert(api.data.message);
+          return api.data;
+        }
+
+
+  return (
+   <AppContext.Provider value={{Users,register,login}}>
+    {
+        props.children
+    }
+   </AppContext.Provider>
+  )
+}
+
+export default AppState
